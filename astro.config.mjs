398e4d527b80865ef69f29e84e037714c9bf1f5e
Salvatore Gerbino <salvatore.gerbino@edu-its.it>
2026-06-04 16:34:48 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,4 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
-
 export default defineConfig({
-    output: 'server',
-    adapter: cloudflare(),
-    // Questa opzione aiuta a gestire i file nella cartella public
-    build: {
-        assets: 'assets'
-    }
+    output: 'static'
 });
