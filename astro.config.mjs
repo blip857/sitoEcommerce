704a07d81c1fbff96f186f6f731ebd20af8a6812
Salvatore Gerbino <salvatore.gerbino@edu-its.it>
2026-06-04 16:21:34 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
     output: 'server',
-    adapter: cloudflare({
-        // Rimuoviamo kvNamespaces qui se lo stiamo già dichiarando in wrangler.json
-        // per evitare che l'adattatore cerchi di crearne uno nuovo.
-    }),
+    adapter: cloudflare(),
+    // Questa opzione aiuta a gestire i file nella cartella public
+    build: {
+        assets: 'assets'
+    }
 });
